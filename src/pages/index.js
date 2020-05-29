b9dc5cdf1df86d30acca289ef0778f24744c6aab
skiprox <seanmichaelscanlan@gmail.com>
2020-05-28 23:14:32 -0400

    Adding in black background, tweaking some mobile style

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ export default function Home(context) {
       <img src="/images/men.png" className="fixed negative-z men"></img>
       <img src="/images/woman1.png" className="fixed negative-z womanOne"></img>
       <figure className="Face negative-z"></figure>
-      <section className="vh-100">
+      <section className="Container vh-100">
         <h4 className="subheader subheader--top absolute w-100 tc">surf systems regional incorporated</h4>
         <img src="/images/logo.png" className="Text-logo absolute"></img>
         <h4 className="subheader subheader--bottom absolute w-100 tc">surf systems regional incorporated</h4>
       </section>
-      <section className="flex justify-center mv5">
+      <section className="Container flex justify-center mv5">
         <div className="w-100 w-60-l tj">
           <p className="body-copy">SSRI is a limited liability company established in the Northeast United States in the year 2020 by Tee Topor and Sean Scanlan, two MFA candidates at the Design & Technology program at Parsons.</p>
         </div>
